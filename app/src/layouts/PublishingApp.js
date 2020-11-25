902975b0321d94b7cb83ab1ee61f12d33fa092c2
Sridhar <crsridhar23gmail.comgit config --global user.email crsridhar23gmail.com>
2020-11-25 22:47:05 +0530

    added falcor

--- app/src/layouts/PublishingApp.js
+++ app/src/layouts/PublishingApp.js
@@ -1,13 +1,31 @@
 import react from 'react';
 import { connect } from 'react-redux';
+import falcorModel from '../falcorModel.js';
+import {bindActionCreators} from 'redux';
+import articleActions from '../actions/article.js';
 
 const mapStateToProps = (state) => ({ ...state });
-const mapDispatchToProps = (dispatch) => ({});
+const mapDispatchToProps = (dispatch) => ({ articleActions: bindActionCreators(articleActions, dispatch)});
 
 class PublishingApp extends React.component {
   constructor(props){
     super(props);
   }
+
+  componentWillMount(){
+    this._fetch();
+  }
+
+  async _fetch(){
+    const articleLength = await falcorModel.getValue('articleLength').then((length) => length);
+
+    const article = await falcorModel.get(['articles', {from: 0, to: articleLength-1}, ['id', 'articleTitle', 'articleContent']]).then((articlesResponse) =>
+  articlesResponse.json.articles);
+    this.props.articleActions.articlesList(articles);
+  }
+
+
+
   render(){
     console.log(this.props);
     let articlesJSX = [];
--- app/src/reducers/article.js
+++ app/src/reducers/article.js
@@ -1,18 +1,9 @@
-const articleMock = {
-  '987654': {
-    articleTtitle: 'Lorem ipsum - article one',
-    articleContent: 'Here goes the content of the article'
-  },
-  '123456': {
-    articleTitle: 'Lorem ipsum - article two',
-    articleContent: 'Sky is the limit, the content goes here.'
-  }
-}
-
-const article = (state = articlesMock, action) => {
-  switch(action.type) {
+const article = (state={}, action) => {
+  switch(action.type){
     case 'RETURN_ALL_ARTICLES':
       return Object.assign({}, state);
+    case 'ARTICLES_ADD_LIST':
+      return Object.assign({}, action.payload.response);
     default:
       return state;
   }
