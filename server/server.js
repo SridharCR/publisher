3c162cd65e3d4e022f746696a193f4efca4f9c1c
Sridhar <crsridhar23@gmail.com>
2021-01-16 23:01:06 +0530

    server side render

--- server/server.js
+++ server/server.js
@@ -6,6 +6,16 @@ import falcor from 'falcor';
 import falcorExpress from 'falcor-express';
 import falcorRouter from 'falcor-router';
 import routes from './routes.js';
+import React from 'react';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import {renderToStaticMarkup} from 'react-dom/server';
+import ReactRouter from 'react-router';
+import {RoutingContext, match} from 'react-router';
+import * as hist from 'history';
+import rootReducer from '../src/reducers';
+import reactRoutes from '../src/routes';
+import fetchServerSide from './fetchServerSide';
 
 const app = express();
 
@@ -20,7 +30,66 @@ app.use('/model.json', falcorExpress.dataSourceRoute(function (req, res) {
   return new falcorRouter(routes);
 }));
 
-app.use(express.static('dist'));
+let handleServerSideRender = (req, res, next) => {
+try {
+let initMOCKstore = fetchServerSide(); // mocked for now
+// Create a new Redux store instance
+const store = createStore(rootReducer, initMOCKstore);
+const location = hist.createLocation(req.path);
+match({
+routes: reactRoutes,
+location: location,
+}, (err, redirectLocation, renderProps) => {
+if (redirectLocation) {
+res.redirect(301, redirectLocation.pathname +
+redirectLocation.search);
+} else if (err) {
+console.log(err);
+next(err);
+// res.send(500, error.message);
+} else if (renderProps === null) {
+res.status(404)
+.send('Not found');
+} else {
+if (typeofrenderProps === 'undefined') {
+// using handleServerSideRender middleware not required;
+// we are not requesting HTML (probably an app.js or other file)
+return;
+}
+let html = renderToStaticMarkup(
+<Provider store={store}>
+<RoutingContext {...renderProps}/>
+</Provider>
+);
+const initialState = store.getState()
+let fullHTML = renderFullPage(html, initialState);
+res.send(fullHTML);
+}
+});
+} catch (err) {
+next(err)
+}
+}
+let renderFullHtml = (html, initialState) =>
+{
+return `<!doctype html>
+<html>
+<head>
+<title>Publishing App Server Side Rendering</title>
+</head>
+<body>
+<h1>Server side publishing app</h1>
+<div id="publishingAppRoot">${html}</div>
+<script>
+window.__INITIAL_STATE__ = ${JSON.stringify(initialState)}
+</script>
+<script src="/static/app.js"></script>
+</body>
+</html>`;
+};
+app.use(handleServerSideRender);
+
+app.use('/static', express.static('dist'));
 
 app.server.listen(process.env.PORT || 3000);
 console.log(`Started on port ${app.server.address().port}`);
